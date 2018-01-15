fc03e63ddeaf5e3fa634bc61286b3ac93b29c8d8
Dario Kampkaspar <dario.kampkaspar@oeaw.ac.at>
2018-01-15 13:14:46 +0100

    load function.js so login works correctly; cf. #56

--- edoc/resources/scripts/admin.js
+++ edoc/resources/scripts/admin.js
@@ -7,19 +7,3 @@ function job ( job, file ) {
 	url = 'projects.html?job=' + job + '&file=' + file;
 	rightSide ( url );
 }
-
-function rightSide ( url ) {
-	html = $.ajax({
-			url: url,
-			cache: false,
-			success: function ( data ) {
-					var result = $('<div/>').append( data ).find( '#data' ).html();
-					$( '#rightSide' ).html( result );
-				}
-		});
-}
-
-/** fixed div at top of page: body needs offset for correct scrolling */
-$(document).ready(function() {
-	$('body').css('margin-top', $('#navBar').innerHeight());
-});
