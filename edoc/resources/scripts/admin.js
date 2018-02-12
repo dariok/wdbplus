49467aa62238013fe77fb57eb8e2c933f515308a
Dario Kampkaspar <dario.kampkaspar@oeaw.ac.at>
2018-02-12 14:16:17 +0100

    Fkt rightSide wieder einführen

--- edoc/resources/scripts/admin.js
+++ edoc/resources/scripts/admin.js
@@ -7,3 +7,14 @@ function job ( job, file ) {
 	url = 'projects.html?job=' + job + '&file=' + file;
 	rightSide ( url );
 }
+
+function rightSide ( url ) {	
+	html = $.ajax({	
+			url: url,	
+			cache: false,	
+			success: function ( data ) {	
+					var result = $('<div/>').append( data ).find( '#data' ).html();	
+					$( '#rightSide' ).html( result );	
+				}	
+		});	
+}
