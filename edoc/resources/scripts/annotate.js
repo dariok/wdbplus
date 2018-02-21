97e93778dbdad914c1f0b0dc95db70bdc7a7e119
Dario Kampkaspar <dario.kampkaspar@oeaw.ac.at>
2018-02-21 14:15:22 +0100

    Annotator als Option eingebaut; closes #83

---
+++ edoc/resources/scripts/annotate.js
@@ -0,0 +1,60 @@
+function anno(){
+    selection = window.getSelection();
+    backwards = (selection.focusNode === selection.getRangeAt(0).startContainer);
+    
+    if (backwards) {
+        end = selection.anchorNode.wholeText.trim() == '' ? selection.anchorNode.previousElementSibling.id
+            : selection.anchorNode.parentNode.id;
+        start = selection.focusNode.wholeText.trim() == '' ? selection.focusNode.nextElementSibling.id
+            : selection.focusNode.parentNode.id;
+        
+        console.log('a: ' + start + ': ' + selection.anchorNode.wholeText);
+        console.log('f: ' + end + ': ' + selection.focusNode.wholeText);
+    } else {
+        start = selection.anchorNode.wholeText.trim() == '' ? selection.anchorNode.nextElementSibling.id
+            : selection.anchorNode.parentNode.id;
+        end = selection.focusNode.wholeText.trim() == '' ? selection.focusNode.previousElementSibling.id
+            : selection.focusNode.parentNode.id;
+    }
+    
+    annoText = window.prompt("Anmerkung", "");
+    id = $("meta[name='id']").attr("content");
+    
+    put = $.get("insert.xql", 
+        {
+            file: id,
+            from: start,
+            to: end,
+            cat: annoText
+        });
+        
+    get = $.getJSON(
+        "return.xql",
+        {file: id},
+        function(data){ console.log(data); }
+    );
+}
+
+$(document).ready(function() {
+    id = $("meta[name='id']").attr("content");
+    get = $.getJSON(
+        "return.xql",
+        {file: id},
+        function(data){
+            console.log(data);
+            $.each(
+                data.entry,
+                function( index, value ) {
+                    if (index > 0) {
+	                    console.log(value.range);
+	                    start = $('#' + value.range["from"]);
+	                    end = $('#' + value.range["to"]);
+	                    cat = value.cat;
+	                    
+	                    highlightAll(start, end, 'red', cat);
+	                }
+                }
+            );
+        }
+    );
+});
--- edoc/return.xql
+++ edoc/return.xql
@@ -1,9 +1,17 @@
 xquery version "3.1";
 
 declare namespace anno = "annotate";
-import module namespace json = "http://www.json.org";
+(:import module namespace json = "http://www.json.org";:)
+declare namespace output = "http://www.w3.org/2010/xslt-xquery-serialization";
+declare option output:method "json";
+declare option output:media-type "application/json";
 
 let $file := request:get-parameter('file', '')
-let $anno := <anno>{doc('/db/apps/edoc/anno.xml')//anno:file[. = $file]/parent::anno:entry}</anno>
+let $anno :=
+	<anno:anno>
+		<anno:entry><anno:range from="" to=""/></anno:entry>
+		{doc('/db/apps/edoc/anno.xml')//anno:file[. = $file]/parent::anno:entry}
+	</anno:anno>
 
-return json:contents-to-json($anno)
+(:return json:contents-to-json($anno):)
+return $anno
