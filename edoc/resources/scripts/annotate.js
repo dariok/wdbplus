e29fe74f51a11b6e17ffe12d446fda3a0bbae748
Dario Kampkaspar <dario.kampkaspar@oeaw.ac.at>
2018-08-20 17:33:04 +0200

    get ID even if another span is contained in word span

--- edoc/resources/scripts/annotate.js
+++ edoc/resources/scripts/annotate.js
@@ -16,7 +16,7 @@ $( function() {
             }),
             contentType: "application/json; charset=UTF-8",
             dataType: 'json'
-        });
+            });
             
         get = $.getJSON(
             "return.xql",
@@ -53,33 +53,35 @@ $( function() {
     });
     
     $('#annButton').on("click", anno);
-    
-    function anno(){selection = window.getSelection();
-        if (selection.focusNode === null && selection.anchorNode === null)
-            return false;
-        
-        backwards = (selection.focusNode === selection.getRangeAt(0).startContainer);
-        
-        if (backwards) {
-            end = selection.anchorNode.wholeText.trim() == '' ? selection.anchorNode.previousElementSibling.id
-                : selection.anchorNode.parentNode.id;
-            start = selection.focusNode.wholeText.trim() == '' ? selection.focusNode.nextElementSibling.id
-                : selection.focusNode.parentNode.id;
-        } else {
-            start = selection.anchorNode.wholeText.trim() == '' ? selection.anchorNode.nextElementSibling.id
-                : selection.anchorNode.parentNode.id;
-            end = selection.focusNode.wholeText.trim() == '' ? selection.focusNode.previousElementSibling.id
-                : selection.focusNode.parentNode.id;
-        }
-        
-        $('#annText').text(selection.toString());
-        $('#annFrom').text(start);
-        $('#annTo').text(end);
-        
-        dialog.dialog("open");
-    }
-});
+//});
 
+function anno(){selection = window.getSelection();
+    if (selection.focusNode === null && selection.anchorNode === null)
+        return false;
+    
+    backwards = (selection.focusNode === selection.getRangeAt(0).startContainer);
+    
+    if (backwards) {
+        end = selection.anchorNode.wholeText.trim() == '' ? selection.anchorNode.previousElementSibling.id
+            : selection.anchorNode.parentNode.id;
+        start = selection.focusNode.wholeText.trim() == '' ? selection.focusNode.nextElementSibling.id
+            : (selection.focusNode.parentNode.id == '' ? selection.focusNode.parentNode.parentNode.id
+                : selection.focusNode.parentNode.id);
+    } else {
+        start = selection.anchorNode.wholeText.trim() == '' ? selection.anchorNode.nextElementSibling.id
+            : (selection.anchorNode.parentNode.id == '' ? selection.anchorNode.parentNode.parentNode.id
+                : selection.anchorNode.parentNode.id);
+        end = selection.focusNode.wholeText.trim() == '' ? selection.focusNode.previousElementSibling.id
+            : selection.focusNode.parentNode.id;
+    }
+    
+    $('#annText').text(selection.toString());
+    $('#annFrom').text(start);
+    $('#annTo').text(end);
+    
+    dialog.dialog("open");
+}
+});
 $(document).ready(function() {
     id = $("meta[name='id']").attr("content");
     get = $.getJSON(
