b51accaa1a41dce9dd6ab4bf74b4b67725f27015
Dario Kampkaspar <dario.kampkaspar@oeaw.ac.at>
2018-08-21 16:27:40 +0200

    use data from cookie for authentication when writing
    Cf. #137

--- edoc/resources/scripts/annotate.js
+++ edoc/resources/scripts/annotate.js
@@ -3,12 +3,19 @@ $( function() {
     
     function parsefta(start, end) {
         id = $("meta[name='id']").attr("content");
+        
+        cred = Cookies.get("wdbplus");
+        headers = "";
+        if (typeof cred !== "undefined" && cred.length != 0)
+        headers = {"Authorization": "Basic " + cred};
+        
         annoText = $('#ftaText').val();
         dialog.dialog("close");
         
         put = $.ajax({
             method: "post",
             url: "../restxq/edoc/anno/" + id,
+            headers: headers,
             data: JSON.stringify({
                 from: start,
                 to: end,
@@ -53,46 +60,51 @@ $( function() {
     });
     
     $('#annButton').on("click", anno);
-//});
-
-function anno(){selection = window.getSelection();
-    if (selection.focusNode === null && selection.anchorNode === null)
-        return false;
     
-    backwards = (selection.focusNode === selection.getRangeAt(0).startContainer);
-    
-    if (backwards) {
-        end = selection.anchorNode.wholeText.trim() == '' ? selection.anchorNode.previousElementSibling.id
-            : selection.anchorNode.parentNode.id;
-        start = selection.focusNode.wholeText.trim() == '' ? selection.focusNode.nextElementSibling.id
-            : (selection.focusNode.parentNode.id == '' ? selection.focusNode.parentNode.parentNode.id
-                : selection.focusNode.parentNode.id);
-    } else {
-        start = selection.anchorNode.wholeText.trim() == '' ? selection.anchorNode.nextElementSibling.id
-            : (selection.anchorNode.parentNode.id == '' ? selection.anchorNode.parentNode.parentNode.id
-                : selection.anchorNode.parentNode.id);
-        end = selection.focusNode.wholeText.trim() == '' ? selection.focusNode.previousElementSibling.id
-            : selection.focusNode.parentNode.id;
+    function anno(){selection = window.getSelection();
+        if (selection.focusNode === null && selection.anchorNode === null)
+            return false;
+        
+        backwards = (selection.focusNode === selection.getRangeAt(0).startContainer);
+        
+        if (backwards) {
+            end = selection.anchorNode.wholeText.trim() == '' ? selection.anchorNode.previousElementSibling.id
+                : selection.anchorNode.parentNode.id;
+            start = selection.focusNode.wholeText.trim() == '' ? selection.focusNode.nextElementSibling.id
+                : (selection.focusNode.parentNode.id == '' ? selection.focusNode.parentNode.parentNode.id
+                    : selection.focusNode.parentNode.id);
+        } else {
+            start = selection.anchorNode.wholeText.trim() == '' ? selection.anchorNode.nextElementSibling.id
+                : (selection.anchorNode.parentNode.id == '' ? selection.anchorNode.parentNode.parentNode.id
+                    : selection.anchorNode.parentNode.id);
+            end = selection.focusNode.wholeText.trim() == '' ? selection.focusNode.previousElementSibling.id
+                : selection.focusNode.parentNode.id;
+        }
+        
+        $('#annText').text(selection.toString());
+        $('#annFrom').text(start);
+        $('#annTo').text(end);
+        
+        dialog.dialog("open");
     }
-    
-    $('#annText').text(selection.toString());
-    $('#annFrom').text(start);
-    $('#annTo').text(end);
-    
-    dialog.dialog("open");
-}
 });
 $(document).ready(function() {
     id = $("meta[name='id']").attr("content");
-    get = $.getJSON(
-        "../restxq/edoc/anno/" + id,
-        {file: id},
-        function(data){
+    cred = Cookies.get("wdbplus");
+    headers = "";
+    if (typeof cred !== "undefined" && cred.length != 0)
+        headers = {"Authorization": "Basic " + cred};
+    
+    get = $.ajax({
+        method: "get",
+        url: "../restxq/edoc/anno/" + id,
+        headers: headers,
+        success: function(data){
             $.each(
                 data.entry,
                 function( index, value ) {
+                    console.log(index + ": " + value);
                     if (index > 0 && value.range["from"] != '') {
-                        console.log(value);
 	                    start = $('#' + value.range["from"]);
 	                    if (value.range["to"] == '' || value.range["to"] == 'undefined')
 	                        end = start;
@@ -106,5 +118,5 @@ $(document).ready(function() {
                 }
             );
         }
-    );
+    });
 });
