a17324d7ecc708e48a76e3ee10426d95f8938317
Dario Kampkaspar <dario.kampkaspar@ulb.tu-darmstadt.de>
2019-08-22 12:20:18 +0200

    make upload work in a sync way
    Cf. https://github.com/dariok/wdbplus/issues/355

--- edoc/resources/scripts/admin.js
+++ edoc/resources/scripts/admin.js
@@ -23,29 +23,29 @@ function rightSide ( url ) {
 
 $('#picker').on("submit", dirupload);
 
-function dirupload (event) {
+async function dirupload (event) {
   event.preventDefault();
   $('#results').children().remove();
   $('p img').show();
-  let files = event.target[0].files;
   
+  let files = event.target[0].files;
   let cred = Cookies.get("wdbplus");
-  let headers = "";
-  if (typeof cred !== "undefined" && cred.length != 0)
-    headers = {"Authorization": "Basic " + cred};
-    
-  for (let i = 0; i < files.length; i++) {
+  let headers = (typeof cred !== "undefined" && cred.length != 0)
+    ? {"Authorization": "Basic " + cred}
+    : "";
+  
+  for (let i=0; i < files.length; i++) {
     let file = files[i];
     let type = file.webkitRelativePath.substring(file.webkitRelativePath.length - 3);
     let content = (type == 'xml' || type == 'xsl') ? "application/xml" : "application/octet-stream";
-    $.ajax({
+    
+    await $.ajax({
       method: "post",
       url: rest + "/admin/ingest/file?name=" + file.webkitRelativePath + "&collection=" + params['collection'],
       headers: headers,
       data: file,
       contentType: content,
       processData: false,
-      async: false,
       success: function (response, textStatus, xhr) {
         console.log(response);
         let li = "<li>" + response + "</li>";
@@ -55,6 +55,7 @@ function dirupload (event) {
         console.log(response);
       }
     });
-  };
+  }
+
   $('p img').hide();
 }
