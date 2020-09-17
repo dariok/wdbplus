7cb56ddd2099e33243abdb3cb3f63ccdee773350
Dario Kampkaspar <dario.kampkaspar@oeaw.ac.at>
2020-09-17 17:43:21 +0200

    fetch changes in admin.js from branch admin
    Cf. #404

--- edoc/resources/scripts/admin.js
+++ edoc/resources/scripts/admin.js
@@ -24,66 +24,6 @@ const wdbAdmin = {
   showJob: function ( job, file ) {
     let url = 'projects.html?job=' + job + '&file=' + file;
     this.displayRight ( url );
-  },
-
-  /* check files in the upload list and prompt upload if everything is okay */
-  prepareForUpload: async function (file, i, fileid, headers) {
-    if (fileid !== "undefined" && fileid !== 0) {
-      try {
-        let task = $('#selectTask input:checked').attr("id"),
-            item = $('#results').children()[i],
-            text = (task == "fi") ? file.name : item.innerText,
-            collection = $('#selectTarget select').val() !== undefined ?
-                $('#selectTarget select').val() :
-                wdb.parameters.collection,
-            delimiter = (wdb.meta.rest.substr(wdb.meta.rest.length - 1)) == '/' ? "" : "/",
-            pathToEd = $('#selectTarget').find('option')[0].innerHTML,
-            edRoot = pathToEd.substr(pathToEd.lastIndexOf('/') + 1),
-            relativeFilePath = task == "fi" ?
-                collection.substr(pathToEd.length + 1) + '/' + text :
-                text.substr(0, edRoot.length) == edRoot ?
-                    text.substr(edRoot.length + 1) :
-                    collection.substr(collection.indexOf('/' + edRoot) + edRoot.length + 1) + '/' + text,
-            mdMode = task == "do" ? "" : "?meta=1";
-        
-        console.info("checking fileid: " + fileid);
-      
-        let formdata = new FormData();
-        formdata.append("file", file)
-          .append("filename", relativeFilePath)
-          .append("targetCollection", collection);
-        
-        // check whether a file file with the given ID exists on the server
-        $.ajax({
-          method: "get",
-          url: wdb.meta.rest + delimiter + "collection/" + collection,
-          dataType: "json",
-          success: function ( response, textStatus, xhr ) {
-            if (xhr.status == 200) {
-              $(item).children("span")[0].innerText = "…";
-              doUpload("put", rest + delimiter + "resource/" + fileid, headers, formdata, item, text);
-            } else {
-              console.log(response);
-              $(item).children("span").innerText = "✕";
-              $(item).children("span").attr("title", "Unexpected return code: " + xhr.status);
-            }
-          },
-          error: function (response) {
-            if (response.status == 404) {
-              $(item).children("span")[0].innerText = "…";
-              doUpload("post", rest + delimiter + "collection/" + params["id"], headers, formdata, item, text);
-            } else {
-              console.log(response);
-              $(item).children("span")[0].innerText = "✕";
-              $(item).children("span").attr("title", "Unexpected return code: " + response.status);
-            }
-          }
-        });
-      }
-    } catch (e) {
-      console.log(e);
-      console.log(e.stack);
-    }
   }
 };
 Object.freeze(wdbAdmin);
@@ -97,14 +37,108 @@ $(document).on("change", "#picker", function() {
   let files = this.files;
   
   for (let file of files) {
-    let path = $('#selectTask input:checked').attr("id") == "fi" ?
-          file.name :
-          file.webkitRelativePath;
-    $('#results').append("<li>" + path + "<span></span></li>");
+    let task = $('#selectTask input:checked').attr("id"),
+        text = task == "fi" ? file.name : files.webkitRelativePath,
+        pathToEd = $('#selectTarget').find('option')[0].innerHTML,
+        edRoot = pathToEd.substr(pathToEd.lastIndexOf('/') + 1),
+        collection = $('#selectTarget select').val() !== undefined ? $('#selectTarget select').val() : wdb.parameters.collection,
+        relPath = task == "fi" ?
+            collection.substr(pathToEd.length + 1) + '/' + text :
+            text.substr(0, edRoot.length) == edRoot ?
+                text.substr(edRoot.length + 1) :
+                collection.substr(collection.indexOf('/' + edRoot) + edRoot.length + 1) + '/' + text;
+    
+    $('#results').append("<li>" + relPath + "<span></span></li>");
   }
 });
 
 
+
+$("document").ready(function() {
+  if (params['id'] !== undefined)
+  {
+    let delim = (rest.substr(rest.length - 1)) == '/' ? "" : "/";
+    let url = rest + delim + "collection/" + params["id"] + "/collections.json";
+    $.ajax({
+      method: "get",
+      url: url,
+      success: function (data) {
+        getPaths(data);
+        $("input[type='submit']").prop("disabled", false);
+        $("aside").html("");
+      },
+      error: function (response) {
+        console.log(response);
+        $("aside").html("<p>Kein Projekt mit der ID " + params["id"] + " gefunden oder Projekt für den aktuellen Benutzer nicht lesbar.</p>");
+      }
+    });
+    $('#selectTarget').show();
+  }
+});
+function getPaths (data) {
+  if (data.hasOwnProperty("path"))
+    $('#selectTarget select').append("<option>" + data["path"] + "</option>");
+  if (data.hasOwnProperty("collection"))
+    if (data.collection instanceof Array) data["collection"].forEach(function(coll) { getPaths(coll); });
+    else $('#selectTarget select').append("<option>" + data.collection["path"] + "</option>");
+}
+
+
+
+
+
+
+async function sendData (file, i, fileid, headers) {
+  try {
+    let task = $('#selectTask input:checked').attr("id"),
+        type = (task == "fi") ? file.name.substr(file.name.length - 3) : file.webkitRelativePath.substring(file.webkitRelativePath.length - 3),
+        content = (type == 'xml' || type == 'xsl') ? "application/xml" : "application/octet-stream",
+        item = $('#results').children()[i],
+        text = (task == "fi") ? file.name : item.innerText,
+        collection = $('#selectTarget select').val() !== undefined ? $('#selectTarget select').val() : params['collection'],
+        delim = (rest.substr(rest.length - 1)) == '/' ? "" : "/",
+        pathToEd = $('#selectTarget').find('option')[0].innerHTML,
+        edRoot = pathToEd.substr(pathToEd.lastIndexOf('/') + 1),
+        relpath = text,
+        mode = task == "do" ? "" : "?meta=1";
+    
+    console.log("fileid: " + fileid);
+    if (fileid !== "undefined" && fileid !== 0) {
+      let formdata = new FormData();
+      formdata.append("file", file);
+      formdata.append("filename", relpath);
+      formdata.append("targetCollection", collection)
+      
+      $.ajax({
+        method: "get",
+        url: rest + delim + "resource/" + fileid,
+        success: function (response, textStatus, xhr) {
+          if (xhr.status == 200) {
+            $(item).children("span")[0].innerText = "…";
+            doUpload("put", rest + delim + "resource/" + fileid, headers, formdata, item, text);
+          } else {
+            console.log(response);
+            $(item).children("span").innerText = "✕";
+            $(item).children("span").attr("title", "Unexpected return code: " + xhr.status);
+          }
+        },
+        error: function (response) {
+          if (response.status == 404) {
+            $(item).children("span")[0].innerText = "…";
+            doUpload("post", rest + delim + "collection/" + params["id"], headers, formdata, item, text);
+          } else {
+            console.log(response);
+            $(item).children("span")[0].innerText = "✕";
+            $(item).children("span").attr("title", "Unexpected return code: " + response.status);
+          }
+        }
+      });
+    }
+  } catch (e) {
+    console.log(e);
+    console.log(e.stack);
+  }
+}
 async function doUpload(method, url, headers, formdata, item, text) {
   $.ajax({
     method: method,
@@ -145,9 +179,8 @@ function dirupload (event) {
       let content = readFile.target.result,
           fileid = 0;
       try {
-        let parser = new DOMParser(),
-            parsed = parser.parseFromString(content, "application/xml"),
-            xml = $(parsed);
+        let parsed = $.parseXML(content.substring(content.indexOf("<TEI")));
+        let xml = $(parsed);
         fileid = xml.find("TEI").attr("xml:id");
       } catch (e) {
         console.log("error parsing XML from " + file.name);
@@ -170,7 +203,13 @@ function dirupload (event) {
 }
 
 function ingestAction(event) {
-  if(event.target.id == "fi") { $('#picker').attr('webkitdirectory', null); }
-  else { $('#picker').attr('webkitdirectory', 'true'); }
+  if(event.target.id == "fi") {
+    $('#picker').attr('webkitdirectory', null);
+    $('#selectInputDir label').text("Datei auswählen");
+  }
+  else {
+    $('#picker').attr('webkitdirectory', 'true');
+    $('#selectInputDir label').text("Verzeichnis auswählen");
+  }
 }
 
