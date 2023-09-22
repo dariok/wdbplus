ec2e30881a2fc21489c228f6a6cf77119ba2c831
Dario Kampkaspar <dario.kampkaspar@tu-darmstadt.de>
2023-09-22 23:55:44 +0200

    search.js must work with entity search results
    evaluate query params to construct links based on job and entity type

--- edoc/resources/scripts/search.js
+++ edoc/resources/scripts/search.js
@@ -1,9 +1,13 @@
 $(() => {
   $(document).on('click', '.loadSearchResult', ( event ) => {
     let id = event.target.dataset.target
-      , q = event.target.dataset.query;
-    if ( q !== undefined ) {
+      , q = event.target.dataset.query
+      , p = JSON.parse(decodeURI(wdb.parameters.p));
+    
+    if ( q !== undefined && p.job == 'fts' ) {
       wdbDocument.loadContent(wdb.meta.rest + 'search/file/' + id + '.html?q=' + q, id, event.target);
+    } else if ( q !== undefined && p.job == 'search' ) {
+      wdbDocument.loadContent(wdb.meta.rest + 'entities/collection/' + wdb.meta.ed + '/' + p.type + '/' + id + '.html', id, event.target);
     } else {
       wdbDocument.loadContent("", id, event.target);
     }
